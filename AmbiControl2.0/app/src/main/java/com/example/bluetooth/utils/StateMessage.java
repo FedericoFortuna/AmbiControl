9ddce85945c15ac9850c4334da0cc410f238befe
LucasPreiti <82056540+LucasPreiti@users.noreply.github.com>
2024-07-06 11:19:58 -0300

    Update StateMessage.java

--- AmbiControl2.0/app/src/main/java/com/example/bluetooth/utils/StateMessage.java
+++ AmbiControl2.0/app/src/main/java/com/example/bluetooth/utils/StateMessage.java
@@ -4,16 +4,19 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 
-public class StateMessage {
+public class StateMessage 
+{
     private static StateMessage instance;
     private final Map<Integer, String> messageMap;
 
-    private StateMessage() {
+    private StateMessage() 
+    {
         messageMap = new HashMap<>();
         configure();
     }
 
-    public static synchronized StateMessage getInstance() {
+    public static synchronized StateMessage getInstance() 
+    {
         if (instance == null) 
         {
             instance = new StateMessage();
@@ -21,7 +24,8 @@ public class StateMessage {
         return instance;
     }
 
-    private void configure() {
+    private void configure() 
+    {
         messageMap.put(0, Constants.TEXT_PREPARADO);
         messageMap.put(1, Constants.TEXT_ILUMINANDO_Y_VENTILANDO);
         messageMap.put(2, Constants.TEXT_DESENERGIZANDO_ACTUADORES);
@@ -30,7 +34,8 @@ public class StateMessage {
         messageMap.put(5, Constants.TEXT_VENTILANDO_POR_BT);
     }
 
-    public String getValue(Integer key) {
+    public String getValue(Integer key) 
+    {
         return Objects.requireNonNull(messageMap.get(key));
     }
 
